a6d9ca2d4ea07db93704b64b0faf547ab55fbad8
joantigua <75149454+joantigua@users.noreply.github.com>
2022-06-08 18:39:55 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebCasosCovid.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
